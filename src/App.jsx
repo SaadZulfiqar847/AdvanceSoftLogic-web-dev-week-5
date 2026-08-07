291d0ea791582fa517c94dc474e40a0dde47ef86
SaadZulfiqar847 <saadhussain575@gmail.com>
2026-08-08 03:41:10 +0500

    Add smooth fade-slide route transition animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Cities from './pages/Cities';
@@ -7,16 +7,23 @@ import Favorites from './pages/Favorites';
 import NotFound from './pages/NotFound';
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cities" element={<Cities />} />
-        <Route path="/cities/:cityName" element={<CityDetail />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      {/* key={location.pathname} forces this div to remount on every
+          route change (including different :cityName values), which
+          restarts the CSS animation defined on .page-transition */}
+      <div className="page-transition" key={location.pathname}>
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cities" element={<Cities />} />
+          <Route path="/cities/:cityName" element={<CityDetail />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </>
   );
 }
